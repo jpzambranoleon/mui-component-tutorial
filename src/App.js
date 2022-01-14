fd6d4ef37ddefa55ca2f849de94f7f878207e985
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-14 17:56:06 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button onClick={() => alert('hello world')} variant="contained" color="secondary">
+        <Button onClick={() => alert('hello')} variant="contained" color="secondary">
           Hello World
         </Button>
         <img src={logo} className="App-logo" alt="logo" />
