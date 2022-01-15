12287eadb797217b9a16f05bf9062f92a6c193e6
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-14 22:26:43 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import { Checkbox } from '@material-ui/core';
 import { FormControlLabel } from '@material-ui/core';
 import { TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B, #FF8E53)',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: 'white',
+    padding: '0 30px'
+  }
+})
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
 
 function CheckboxExample() {
   const [checked, setChecked] = useState(true)
@@ -31,6 +48,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonStyled />
         <TextField 
           variant="filled"
           color="secondary"
