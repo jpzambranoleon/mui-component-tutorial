cdd8e424d327fe8ed974c8d368865e0058078b9e
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-14 18:22:53 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { ButtonGroup } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save'
 
@@ -7,17 +8,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button 
-          onClick={() => alert('hello')}
-          startIcon={<SaveIcon />}
-          size="large"
-          style = {{
-            fontSize: 12
-          }} 
-          variant="contained" 
-          color="secondary">
-          Hello World
-        </Button>
+        <ButtonGroup>
+          <Button 
+            onClick={() => alert('hello')}
+            startIcon={<SaveIcon />}
+            size="large"
+            style = {{
+              fontSize: 12
+            }} 
+            variant="contained" 
+            color="secondary">
+            Hello World
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
 
       </header>
